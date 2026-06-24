a17148b694bf0b3adb320aa0ab5a12dd611c716a
Rishabhkasyap <rishabhkumar7.rs@gmail.com>
2026-06-24 19:24:12 +0530

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,30 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react(),
-    tailwindcss(
-     {
-        keyframes:
-        {
-          Blob:{
-            '0%':{tranform: 'scale(1)'},
-            '33%':{tranform: 'scale(1)'},
-            '66%':{tranform: 'scale(1)'},
-            '100%':{tranform: 'scale(1)'},
-
-          },
-        },
-        animation:{
-          Blob:'blob 10s infinite',
-        }
-      }
-    ),
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
 })
-=======
-  plugins: [react(), tailwindcss()],
-})
->>>>>>> 120b535 (re design portfolio)
